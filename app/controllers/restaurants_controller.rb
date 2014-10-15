b6e2083e05d0d896f95be072deb74629fec55bbf
Alan Venegas <alan.noel.venegas@gmail.com>
2014-10-15 08:08:14 +0200

    New and create Restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,12 +9,21 @@ class RestaurantsController < ApplicationController
 	end
 
 	def new
+		@restaurant = Restaurant.new
 	end
 
 	def create
+		@restaurant = Restaurant.new(restaurant_params)
+		if @restaurant.save
+			redirect_to @restaurant
+		else 
+			redirect_to root_path
+		end
 	end
 
 	def edit
+		@restaurant = Restaurant.find(params[:id])
+
 	end
 	
 	def update
@@ -23,4 +32,9 @@ class RestaurantsController < ApplicationController
 	def destroy
 	end
 
+	private
+
+		def restaurant_params
+			params.require(:restaurant).permit(:name, :description, :address, :phone)
+		end
 end
