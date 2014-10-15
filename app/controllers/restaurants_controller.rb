2e12684ef856bf07f9a20f4277b4c0071f0a0511
Alan Venegas <alan.noel.venegas@gmail.com>
2014-10-15 15:47:25 +0200

    Add flash

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -15,36 +15,41 @@ class RestaurantsController < ApplicationController
 	def create
 		@restaurant = Restaurant.new(restaurant_params)
 		if @restaurant.save
+			flash[:notice] = "Restaurant created"
 			redirect_to @restaurant
-		else 
+		else
+			flash[:error] = "Restaurant created"
 			redirect_to root_path
 		end
+
 	end
 
 	def edit
 		@restaurant = Restaurant.find(params[:id])
 	end
-	
+
 	def update
 		@restaurant = Restaurant.find(params[:id])
 
 		if @restaurant.update_attributes(restaurant_params)
-      redirect_to @restaurant
-    else
-      redirect_to edit_restaurant_path(@restaurant)
-    end
+			redirect_to @restaurant
+		else
+			redirect_to edit_restaurant_path(@restaurant)
+		end
 
 	end
 
 	def destroy
 		restaurant = Restaurant.find(params[:id])
-		restaurant.destroy
+		if restaurant.destroy
+			flash[:notice] = "Restaurant destroyed"
+		end
 		redirect_to root_path
 	end
 
 	private
 
-		def restaurant_params
-			params.require(:restaurant).permit(:name, :description, :address, :phone)
-		end
+	def restaurant_params
+		params.require(:restaurant).permit(:name, :description, :address, :phone)
+	end
 end
