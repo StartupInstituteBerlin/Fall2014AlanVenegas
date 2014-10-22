26caccb9a2d0c6afa6e713734739e9e3a9c1a638
Alan Venegas <alan.noel.venegas@gmail.com>
2014-10-22 14:56:43 +0200

    Improve Restaurant controller testing

--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -18,13 +18,32 @@ RSpec.describe RestaurantsController, :type => :controller do
 	end
 
 	describe 'GET #edit' do
+		before do
+			@request.env["devise.mapping"] = Devise.mappings[:user]
+
+		end
 		context 'when not logged in' do
 			it 'redirects to index' do
-				@request.env["devise.mapping"] = Devise.mappings[:user]
-
 				get :edit, id: @restaurant.id
+				expect(response).to redirect_to(new_user_session_path)
 
-				expect(response).to have_http_status(302)
+			end
+		end
+		context 'when logged in' do
+			context 'trying to access his own restaurant' do
+				it 'returns http 200' do
+					sign_in @restaurant.user
+					get :edit, id: @restaurant.id
+					expect(response).to have_http_status(200)
+				end
+			end
+			context 'trying to access a restaurant he does not own' do
+				it 'redirects to index' do
+					sign_in @user
+					get :edit, id: @restaurant.id
+					expect(response).to redirect_to(restaurant_path(@restaurant))
+
+				end
 			end
 		end
 	end
