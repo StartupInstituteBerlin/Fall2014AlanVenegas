9161b702d3734502b3d893f521dda84679951deb
Alan Venegas <alan.noel.venegas@gmail.com>
2014-10-27 15:19:47 +0100

    Add validations to Reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,7 @@
 class Reservation < ActiveRecord::Base
 	belongs_to :restaurant
+	validates :email, presence: true
+	validates :date_and_time, presence: true
+	validates :restaurant_id, presence: true
+
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, :type => :model do
-	before { @reservation = Reservation.new() }
+	before (:each) do
+		@date = DateTime.now + 1
+		@restaurant = FactoryGirl.create(:restaurant)
+		@reservation = Reservation.new(email: "aaa@bbb.ccc", date_and_time: @date, message: "Thanks", restaurant_id: @restaurant.id) 
+	end
 
 	subject { @reservation }
 
@@ -9,5 +13,26 @@ RSpec.describe Reservation, :type => :model do
 	it { is_expected.to respond_to(:date_and_time ) }
 	it { is_expected.to respond_to(:message) }
 	it { is_expected.to respond_to(:restaurant) }
+
+	describe "when email is not present" do
+		before do 
+			@reservation.email = ""
+		end
+		it { is_expected.to be_invalid}
+	end
+
+	describe "when date-time is not present" do
+		before do 
+			@reservation.date_and_time = nil
+		end
+		it { is_expected.to be_invalid}
+	end
+
+	describe "when not associated to a restaurant" do
+		before do 
+			@reservation.restaurant = nil
+		end
+		it { is_expected.to be_invalid}
+	end
 
 end
