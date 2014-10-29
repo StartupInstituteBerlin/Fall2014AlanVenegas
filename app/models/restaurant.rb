618d3d8fee5297fe76c8e0e104a79a2a9b73e72f
Alan Venegas <alan.noel.venegas@gmail.com>
2014-10-29 12:31:30 +0100

    Show Reservations when the owner checks his Restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,5 @@ class Restaurant < ActiveRecord::Base
 	validates :name, presence: true
 	validates_length_of :name, minimum: 3
 	validates :user_id, presence: true
+	accepts_nested_attributes_for :reservations
 end
