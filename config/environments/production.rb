74b7397a486b820c1dd55f242e5a9b9abd153ba0
Alan Venegas <alan.noel.venegas@gmail.com>
2014-11-05 14:36:35 +0100

    Fix production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,8 @@ RestaurantApp::Application.configure do
   # Custom configuration for mail
   config.action_mailer.delivery_method = :smtp
   
+  config.action_mailer.default_url_options = { host: 'al-restaurant.herokuapp.com' }
+
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
