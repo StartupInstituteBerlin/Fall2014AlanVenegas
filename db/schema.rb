e2e4b106b6a20b8ff53a00ce15324cdee48230f3
Alan Venegas <alan.noel.venegas@gmail.com>
2014-11-10 11:49:56 +0100

    Add Star resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105111912) do
+ActiveRecord::Schema.define(version: 20141110103617) do
 
   create_table "reservations", force: true do |t|
     t.string   "email"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20141105111912) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+  end
+
+  create_table "stars", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "restaurant_id"
+    t.datetime "created"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
---
+++ spec/factories/stars.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :star do
+    created "31/11/2014 10:00:00"
+    restaurant
+    user
+  end
+end
