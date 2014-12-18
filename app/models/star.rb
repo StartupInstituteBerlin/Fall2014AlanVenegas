62612b98a7030ce10bd88ba1c77ba08dedd8fffb
Alan Venegas <alan.noel.venegas@gmail.com>
2014-12-18 14:35:16 +0100

    Fixed bug with form_for pluralization of single resource

--- app/models/star.rb
+++ app/models/star.rb
@@ -2,5 +2,5 @@ class Star < ActiveRecord::Base
 	belongs_to :restaurant
 	belongs_to :user
 	validates :restaurant_id, uniqueness: {scope: :user_id}
-
+  model_name.instance_variable_set(:@route_key, 'star')
 end
